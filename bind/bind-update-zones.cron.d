b18e77046b5e550de4ae2887e42fcddf813792a9
Martin Skuta <mskuta@posteo.net>
2020-12-05 16:45:13 +0100

    bind: Update lists more often
    Thanks to the `--timestamping` option of wget2, lists are only
    downloaded if they have changed, thereby avoiding unnecessary traffic.

--- bind/bind-update-zones.cron.d
+++ bind/bind-update-zones.cron.d
@@ -1,2 +1,2 @@
 SHELL=/bin/bash
-@weekly root set -o pipefail && /usr/local/bin/bind-update-zones 2>&1 | /usr/bin/logger -t bind-update-zones && /usr/sbin/rndc reload
+@hourly root set -o pipefail && /usr/local/bin/bind-update-zones 2>&1 | /usr/bin/logger -t bind-update-zones && /usr/sbin/rndc reload
